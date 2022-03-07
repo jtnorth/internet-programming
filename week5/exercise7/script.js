25856f3e1e6b10c077a8b206cb76ae8ed14a9783
jtnorth <jovdevjob@gmail.com>
2022-03-07 16:41:38 +0000

    [FEAT][Week 5][Exercise 7] Add script to exercise 7

---
+++ week5/exercise7/script.js
@@ -0,0 +1,23 @@
+
+function attachCircleListener(){
+
+    setInterval(function (){
+        const circle = document.getElementById('circle');
+
+        if(circle.style.opacity>0)
+            circle.style.opacity = `${parseFloat(circle.style.opacity) - .01}`;
+        else
+            clearInterval()
+    },10)
+
+
+}
+
+
+function attachListeners(){
+    const circle = document.getElementById('circle');
+    circle.style.opacity = '1'
+    circle.addEventListener('click',attachCircleListener)
+}
+
+document.addEventListener('DOMContentLoaded',attachListeners)
