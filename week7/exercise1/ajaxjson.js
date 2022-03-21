87f18e35c40b8f331cbfccf456676521cb8fdd40
jtnorth <jovdevjob@gmail.com>
2022-03-21 23:18:12 +0000

    [FEAT][Week 7][Exercise 1 & 2] Add exercise 1 and 2

---
+++ week7/exercise1/ajaxjson.js
@@ -0,0 +1,38 @@
+function makeAjaxRequest() {
+    if (window.XMLHttpRequest) {
+        xhr = new XMLHttpRequest();
+    } else if (window.ActiveXObject) {
+        xhr = newActiveXObject("Microsoft.XMLHTTP");
+    }
+    if (xhr) {
+        xhr.open("GET", "counties.json", true);
+        xhr.send();
+        xhr.onreadystatechange = showContents;
+    } else {
+        document.getElementById("updatemessage").innerHTML = "Could not perform stated Request";
+    }
+}
+
+function showContents() {
+    if (xhr.readyState === 4) {
+        let txt = "";
+        if (xhr.status === 200) {
+            let {counties} = JSON.parse(xhr.responseText);
+            counties && counties.forEach((county) => {
+                txt += "<tr><td>" + county.name + "</td></tr>";
+            })
+        }
+
+        document.getElementById("countylist").innerHTML = txt;
+
+        //clear error message in case of success state after error
+        document.getElementById("updatemessage").innerHTML = "";
+    } else {
+        document.getElementById("updatemessage").innerHTML = "An error occurred: " + xhr.status;
+        //clear table in case of error state after success
+        document.getElementById("countylist").innerHTML = "";
+
+    }
+}
+
+window.onload = makeAjaxRequest;
---
+++ week7/exercise2/ajaxjson.js
@@ -0,0 +1,16 @@
+function makeAjaxRequest() {
+
+    $.getJSON( "counties.json", function( data ) {
+        const {counties} = data;
+        let txt = "";
+        counties && counties.forEach((county) => {
+            txt += "<tr><td>" + county.name + "</td></tr>";
+        });
+        $('#countylist').html(txt);
+
+    }).fail(function(error) { $('#updatemessage').text(`An error occurred: ${JSON.stringify(error)}`) })
+}
+
+$( document ).ready(function() {
+    makeAjaxRequest()
+})
